4e7c86718742d6d5b4d9ec65d89061cd1e965d04
TuNombre <fmsaquinaula@sudamericano.edu.ec>
2025-05-13 10:47:41 -0500

    commit

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { UserFormComponent } from './components/user-form/user-form.component'; // Verifica que esta ruta sea correcta
 
 @NgModule({
+  declarations: [
+    UserFormComponent // Declarar el componente
+  ],
   imports: [
-    CommonModule // Importar CommonModule si es necesario para otros componentes
+    CommonModule,
+    ReactiveFormsModule // Importar ReactiveFormsModule para formularios reactivos
+  ],
+  exports: [
+    UserFormComponent // Exportar el componente para usarlo fuera del módulo
   ]
 })
 export class UserModule {}
